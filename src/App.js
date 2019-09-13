45114c472dc82667f5eeeacf34bf415cd3c9d057
DESKTOP-JPA6G9R\artur <artur_olivs@hotmail.com>
2019-09-13 20:58:42 -0300

    Feature - Image Import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './App.css'
 
+import profile from './assets/profile.png'
+
 function App() {
-  return <h1>adad</h1>
+  return <img src={profile} />
 }
 
 export default App
